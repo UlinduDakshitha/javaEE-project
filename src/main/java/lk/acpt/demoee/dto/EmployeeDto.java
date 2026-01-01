ae9f1b803b1ed2921bdc29860852e0541c98df3c
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-01-01 20:20:23 +0530

    update

--- src/main/java/lk/acpt/demoee/dto/EmployeeDto.java
+++ src/main/java/lk/acpt/demoee/dto/EmployeeDto.java
@@ -2,12 +2,15 @@ package lk.acpt.demoee.dto;
 
 public class EmployeeDto {
 
+
     private String nic;
     private String name;
     private int age;
     private double salary;
 
+
     public EmployeeDto() {}
+
 
     public EmployeeDto(String nic, String name, int age, double salary) {
         this.nic = nic;
@@ -15,6 +18,7 @@ public class EmployeeDto {
         this.age = age;
         this.salary = salary;
     }
+
 
     public String getNic() { return nic; }
     public void setNic(String nic) { this.nic = nic; }
